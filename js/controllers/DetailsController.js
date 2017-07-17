849b95891fc4cd7f56595403d7b2f08bcecfc166
William <wilfuentes0320@gmail.com>
2017-07-17 11:43:00 -0600

    final donation detail modal

--- js/controllers/DetailsController.js
+++ js/controllers/DetailsController.js
@@ -6,11 +6,18 @@
         //Nuevos servicios y repositorios
         detailsController.$inject = [
             "$scope",
-            "donationDetails"
+            "donationDetails",
+            "$uibModalInstance"
         ];
 
-        function detailsController($scope, donationDetails){
-            console.log(donationDetails);
+        function detailsController($scope, donationDetails, $uibModalInstance){
+            var vm = this;
+            vm.donation = donationDetails;
+            vm.cancel = cancel;
+            function cancel() {
+                $uibModalInstance.dismiss('cancel');
+            };
+            
         }
 
 })();
