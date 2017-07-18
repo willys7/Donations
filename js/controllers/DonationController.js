6d2d39d007b1f9122531d2b965df37ad58b7903e
William <wilfuentes0320@gmail.com>
2017-07-18 10:17:29 -0600

    finish payment type information

--- js/controllers/DonationController.js
+++ js/controllers/DonationController.js
@@ -15,12 +15,41 @@
             var vm = this;
             vm.paymentLabels = {}
             vm.amountOptions = {}
+            vm.year = [];
+            for (var i=2017; i<=2025; i++){
+                vm.year.push(i);
+            }
+            vm.month = [];
+            for (var i=1; i<=12; i++){
+                vm.month.push(i);
+            }
+            
             getPaymentLabels();
             function getPaymentLabels(){
                 PaymentService.getPaymentTypeLabels().then(function(data){
                     vm.paymentLabels = data;
                     vm.amountOptions = data.AmountQuestions[0].AmountOptions;
+                    var creditCardList = data.CreditCardTypeList;
+                    vm.creditCardValues = [];
+                    for(var i=0; i<creditCardList.length; i++){
+                        if(creditCardList[i] == 1){
+                            vm.creditCardValues.push("Visa")
+                        }
+                        if(creditCardList[i] == 2){
+                            vm.creditCardValues.push("Master Card")
+                        }
+                        if(creditCardList[i] == 3){
+                            vm.creditCardValues.push("American Express")
+                        }
+                        if(creditCardList[i] == 4){
+                            vm.creditCardValues.push("Discover")
+                        }
+                        if(creditCardList[i] == 5){
+                            vm.creditCardValues.push("Diners Club")
+                        }
+                    }
                     console.log(data);
+                    console.log(vm.creditCardValues)
                 }).catch(function(err){
                     console.log(err);
                 });
