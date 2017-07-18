0b105b2af5d8655ca1f522deb9caf00cadc05434
William <wilfuentes0320@gmail.com>
2017-07-18 09:42:38 -0600

    finish payment information

--- js/controllers/DonationController.js
+++ js/controllers/DonationController.js
@@ -14,10 +14,12 @@
         function donationController($scope, AuthService, DonationService, PaymentService){
             var vm = this;
             vm.paymentLabels = {}
+            vm.amountOptions = {}
             getPaymentLabels();
             function getPaymentLabels(){
                 PaymentService.getPaymentTypeLabels().then(function(data){
                     vm.paymentLabels = data;
+                    vm.amountOptions = data.AmountQuestions[0].AmountOptions;
                     console.log(data);
                 }).catch(function(err){
                     console.log(err);
