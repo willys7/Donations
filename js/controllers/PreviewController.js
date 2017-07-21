76cdc606d8a6ab576c73b8aa58d7d1ec0b4cb848
William <wilfuentes0320@gmail.com>
2017-07-21 08:07:59 -0600

    correct data in preview view

--- js/controllers/PreviewController.js
+++ js/controllers/PreviewController.js
@@ -17,5 +17,22 @@
             var vm = this;
             vm.payment = DonationService.getPaymentConfig();
             vm.agency = DonationService.getAgencyDetails();
+            vm.today = new Date();
+            var dd = vm.today.getDate();
+            var mm = vm.today.getMonth()+1; //January is 0!
+
+            var yyyy = vm.today.getFullYear();
+            if(dd<10){
+                dd='0'+dd;
+            } 
+            if(mm<10){
+                mm='0'+mm;
+            } 
+            vm.todayDate = "Billed on " + dd+'/'+mm+'/'+yyyy;
+            PaymentService.getPaymentTypeLabels().then(function(data){
+                vm.info = data;
+            }).catch(function(err){
+                console.log(err);
+            });
         }
 })();
