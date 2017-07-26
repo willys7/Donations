619a536099a57cc9109805ef1c52d6ab59e2367a
William <wilfuentes0320@gmail.com>
2017-07-26 10:25:44 -0600

    add function to register modals in config

--- js/config.js
+++ js/config.js
@@ -40,4 +40,65 @@
         $stateProvider.state(previewState);
         $urlRouterProvider.otherwise("/login");
     }
+
+    var registerStatefulModal = function (url, stateName, controller, template, parent, params, size, resolves) {
+        params = params || {};
+        resolves = resolves || angular.extend({},
+                resolves,
+                {
+                    stateParams: ['$stateParams', function ($stateParams) {
+                        return $stateParams;
+                    }]
+                });
+        size = size || 'md';
+
+        var modal;
+        $stateProvider.state(stateName,
+            {
+                url: url,
+                modal: true,
+                parent: parent || 'home',
+                params: params,
+                onEnter: [
+                    '$stateParams',
+                    '$uibModal',
+                    '$previousState',
+                    function ($stateParams, $uibModal, $previousState) {
+                        //log('stateful modal enter', stateName, resolves);
+                        $previousState.memo('modalInvoker');
+                        modal = $uibModal.open({
+                            animation: true,
+                            templateUrl: template,
+                            controller: controller,
+                            controllerAs: 'vm',
+                            backdrop: 'static',
+                            keyboard: false,
+                            size: size,
+                            resolve: resolves
+                        });
+                        // $scope.cancel functions should use modal.dismiss('cancel') or modal.dismiss('success') etc. to close the modal
+                        // so we can restore the previous state
+                        modal.result.catch(function (reason) {
+                            // log('modal dismiss', reason);
+                            if (reason)
+                                $previousState.go('modalInvoker');
+                        });
+                        modal.result.then(function (result) {
+                            //log('modal close', result);
+                        });
+                        modal.result.finally(function () {
+                            modal.$destroy();
+                        });
+                    }
+                ],
+                onExit: function () {
+                    if (modal) {
+                        modal.close();
+                    }
+                }
+            });
+    };
+    
+    
+
 })();
